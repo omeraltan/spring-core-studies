0ce97def98b3709719f8c9d5ebe72eb99fdd8763
omeraltan <omeraltan66@gmail.com>
2024-01-31 08:49:12 +0300

    Firs Commit about studies

---
+++ studies/src/main/java/com/spring/studies/StudiesApplication.java
@@ -0,0 +1,13 @@
+package com.spring.studies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudiesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudiesApplication.class, args);
+    }
+
+}
