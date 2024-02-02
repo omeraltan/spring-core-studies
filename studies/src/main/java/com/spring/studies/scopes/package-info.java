1939479391e16d0ce5b45b2be7e2524c0f23f50a
omeraltan <omeraltan66@gmail.com>
2024-02-02 08:42:23 +0300

    Sixth Commit

---
+++ studies/src/main/java/com/spring/studies/scopes/package-info.java
@@ -0,0 +1,14 @@
+package com.spring.studies.scopes;
+
+/**
+ *      The scope of a bean defines the life cycle and visibility of that bean in the contets we use it.
+ *      1. singleton
+ *      2. prototype
+ *      3. request
+ *      4. session
+ *      5. application
+ *      6. websocket
+ *
+ *      The last four scopes mentioned, "request", "session", "application" and "websocket" are
+ *      only available in a web-aware application
+ */
