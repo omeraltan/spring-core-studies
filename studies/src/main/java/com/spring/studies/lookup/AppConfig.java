4a65c5dab2ae9472dbf022fe46b0382e95bb1b58
omeraltan <omeraltan66@gmail.com>
2024-02-05 11:12:08 +0300

    9th commit

---
+++ studies/src/main/java/com/spring/studies/lookup/AppConfig.java
@@ -0,0 +1,9 @@
+package com.spring.studies.lookup;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "com.spring.studies.lookup")
+public class AppConfig {
+}
---
+++ studies/src/main/java/com/spring/studies/lookup/package-info.java
@@ -0,0 +1,12 @@
+package com.spring.studies.lookup;
+
+
+/**
+ *
+ *      @Lookup
+ *      In Spring, the Lookup Method Injection is intended for use when
+ *      we want to work with two beans of different lifecycle scopes (singleton, prototype etc.).
+ *      Do not use this when beans are in the same lifecycle scopes.
+ *
+ *
+ */
